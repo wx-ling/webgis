9f825a54cc39c8bd605ce89951864b05776af114
wx-ling <1838586804@qq.com>
2022-02-15 11:15:16 +0800

    初始化项目结构

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <span>sssss</span>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+html,body,#app {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+}
+</style>
